ece45c72afb8da70a6f1717606a15b0381c00157
Sven Lito <me@svenlito.com>
2017-02-10 15:01:35 +0800

    wrap fields in fieldset

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,8 +8,9 @@ const styles = {
     backgroundColor: 'transparent',
     position: 'relative',
     padding: '10px 15px',
-    border: '1px solid',
-    marginBottom: '-1px',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    marginBottom: '5px',
     transition: '0.2s',
     color: 'inherit',
     opacity: '0.4',
@@ -17,69 +18,68 @@ const styles = {
       opacity: '1',
       zIndex: '1',
       transform: 'scale(1.01)',
-      backgroundColor: colors.white
+      backgroundColor: colors.black
     }
   },
   button: {
     backgroundColor: 'transparent',
-    color: 'inherit',
+    color: colors.whiteDark20,
     width: 'auto',
     borderWidth: '2px',
     borderStyle: 'solid',
-    borderColor: 'inherit',
-    marginTop: '5px',
-    marginBottom: '20px',
+    borderColor: colors.darkGray,
     padding: '8px 25px 8px 35px',
     transition: '0.2s',
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
-  black: {
+  white: {
     ':hover': {
-      backgroundColor: colors.white,
-      color: colors.gray,
-      borderColor: colors.black
+      color: colors.pink
     }
   }
 };
-
 const ContactForm = (props) => {
-  const domain = window.location.host;
-  const url = `http://${domain}/thanks`;
+  const url = `http://${window.location.host}/thanks`;
 
   return (
     <form
       action='https://formspree.io/me@svenlito.com'
       method='POST'
     >
-      <input
-        key='1'
-        style={styles.field}
-        type='text'
-        name='name'
-        placeholder='Full Name'
-      />
-      <input
-        key='2'
-        style={styles.field}
-        type='email'
-        name='_replyto'
-        placeholder='Email Address'
-      />
-      <textarea
-        key='3'
-        style={styles.field}
-        type='message'
-        name='message'
-        placeholder='Message'
-      />
-      <input
-        style={[
-          styles.button,
-          styles[props.color]
-        ]}
-        type='submit'
-        value='Send message &rarr;'
-      />
+      <fieldset>
+        <input
+          key='1'
+          style={styles.field}
+          type='text'
+          name='name'
+          placeholder='Full Name'
+          required
+        />
+        <input
+          key='2'
+          style={styles.field}
+          type='email'
+          name='_replyto'
+          placeholder='Email Address'
+          required
+        />
+        <textarea
+          key='3'
+          style={styles.field}
+          type='message'
+          name='message'
+          placeholder='Message'
+          required
+        />
+        <input
+          style={[
+            styles.button,
+            styles[props.color]
+          ]}
+          type='submit'
+          value='Send message &rarr;'
+        />
+      </fieldset>
       <input type='hidden' name='_next' value={url} />
     </form>
   );
