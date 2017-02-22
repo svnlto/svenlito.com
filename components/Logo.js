a3e890c4aa9a4b77d2949cd22b4ae9cd7fe7ed6f
Sven Lito <me@svenlito.com>
2017-02-22 15:23:58 +0800

    fixed logo gradient

--- components/Logo.js
+++ components/Logo.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Radium from 'radium';
 
 import Link from './Link';
-
-import { colors } from '../constants';
 
 const styles = {
   logo: {
@@ -11,22 +9,45 @@ const styles = {
   }
 };
 
-const Logo = () => {
-  return (
-    <Link style={styles.logo} href='/'>
-      <svg width='50px' height='50px' viewBox='0 0 50 50' version='1.1'>
-        <defs>
-          <rect id='path-1' x='30' y='30' width='35' height='35' />
-          <rect id='path-3' x='15' y='15' width='40' height='40' />
-          <rect id='path-5' x='0' y='0' width='50' height='50' />
-        </defs>
-        <use stroke={colors.greenLight10} strokeWidth='1' fill='none' opacity='0.55' xlinkHref='#path-1' />
-        <use stroke={colors.greenLight10} strokeWidth='1' fill='none' opacity='0.6' xlinkHref='#path-3' />
-        <use stroke={colors.greenLight10} strokeWidth='1' fill='none' opacity='0.9' xlinkHref='#path-5' />
-      </svg>
-    </Link>
-  );
-};
+// const increment = (step, speed) => step + speed;
+
+class Logo extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gradient: {
+        from: 'rgb(43, 154, 129)',
+        to: 'rgb(239, 175, 231)'
+      }
+    };
+  }
+
+  render() {
+    const { gradient } = this.state;
+
+    return (
+      <Link style={styles.logo} href='/'>
+        <svg width='50px' height='50px' viewBox='0 0 50 50' version='1.1'>
+          <defs>
+            <linearGradient x1='21%' y1='33%' x2='19%' y2='76%' id='gradient'>
+              <stop offset='5%' stopColor={gradient.from} />
+              <stop offset='95%' stopColor={gradient.to} />
+            </linearGradient>
+            <rect id='path-1' x='30' y='30' width='35' height='35' />
+            <rect id='path-3' x='15' y='15' width='40' height='40' />
+            <rect id='path-5' x='0' y='0' width='50' height='50' />
+          </defs>
+          <use stroke={"url('#gradient')"} strokeWidth='1' fill='#23282c' xlinkHref='#path-1' />
+          <use stroke={"url('#gradient')"} strokeWidth='1' fill='none' xlinkHref='#path-3' />
+          <use stroke={"url('#gradient')"} strokeWidth='2' fill='none' xlinkHref='#path-5' />
+        </svg>
+      </Link>
+    );
+  }
+
+}
 
 export default Radium(Logo);
 
