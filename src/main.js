5b45ccee67607b9944bcae35c4c3a47cf806f092
svnlto <me@svenlito.com>
2019-01-05 15:17:59 +0700

    feat(GA): adds GA tracking

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import VueMq from 'vue-mq';
 
 import App from './App.vue';
@@ -6,6 +7,17 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+const isProd = process.env.NODE_ENV === 'production';
+
+Vue.use(VueAnalytics, {
+  id: 'UA-30900799-1',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+});
 
 Vue.use(VueMq, {
   breakpoints: {
