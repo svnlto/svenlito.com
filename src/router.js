20fe2c2df9fc76e154fb01fc6e6fa37221d565bf
svnlto <me@svenlito.com>
2019-01-05 14:51:25 +0700

    fix(router): determine correct base url

--- src/router.js
+++ src/router.js
@@ -12,7 +12,10 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base:
+    process.env.NODE_ENV === 'production'
+      ? process.env.URL
+      : process.env.BASE_URL,
   routes: [
     {
       path: '/',
