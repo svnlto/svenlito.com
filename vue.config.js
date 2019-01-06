8b777638bd86e414ab7c519090ecb94db245f061
svnlto <me@svenlito.com>
2019-01-06 15:03:01 +0700

    forms

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,15 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin');
 module.exports = {
   lintOnSave: true,
   baseUrl: process.env.NODE_ENV === 'production' ? process.env.URL : '/',
-  // configureWebpack: () => {
-  //   if (process.env.NODE_ENV !== 'production') return;
-  //   return {
-  //     plugins: [
-  //       new PrerenderSPAPlugin({
-  //         staticDir: path.join(__dirname, 'dist'),
-  //         routes: ['/contact']
-  //       })
-  //     ]
-  //   };
-  // }
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/contact']
+        })
+      ]
+    };
+  }
 };
